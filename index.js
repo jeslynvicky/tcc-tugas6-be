d309441090d69b60e9b9b06f2cbe9b717b6a878b
Jeslyn Vicky H. <162588624+jeslynvicky@users.noreply.github.com>
2025-04-03 13:34:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ import cors from "cors";
 import Route from "./routes/Route.js";
 
 const app = express();
+app.set("view engine", "ejs");
+
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => res.render("index"));
 app.use(Route);
 
 app.listen(3000, ()=>console.log('Server up and running . . . '));
